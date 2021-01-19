4596504e436993c9b5bb977973f760ec615074c1
yehk63525 <yehk63525@gmail.com>
2021-01-19 21:49:19 +0800

    create project

--- src/test/java/text.java
+++ src/test/java/text.java
@@ -1,5 +1,5 @@
 public class text {
     public static void main(String[] args){
-        System.out.println("ssssssssssdddssdsssssssss");
+        System.out.println("ssssssssssdddssddddsssssssss");
     }
 }
